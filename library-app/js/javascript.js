9344c536aa9c40c578a027ccacb5071fced9f22d
kyle <kyleb44@hotmail.com>
2025-01-04 08:18:56 -0500

    update js functions add styling to css

--- library-app/js/javascript.js
+++ library-app/js/javascript.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const container = document.getElementById("main");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -16,6 +17,33 @@ function addBookToLibrary(title,author,pages,read) {
 }
 
 
+function createCard (book){
+    const card = document.createElement("div");
+    const classList = ["p-6", "border", "border-slate-300", 
+        "bg-white", "rounded-xl", "shadow-xl", "flex", "flex-col", "gap-x-4"];
+    card.classList.add(...classList);
+    
+
+    Object.keys(book).forEach((element) => {
+        if (element == 'title') {
+            let item = document.createElement('h2');
+            item.textContent = `${book[element]}`;
+            item.classList.add(...["font-bold", "text-2xl"])
+            card.appendChild(item);
+        } else {
+            let item = document.createElement('p');
+            item.textContent = `${element}: ${book[element]}`;
+            card.appendChild(item);
+        }
+    })
+
+    container.appendChild(card);
+
+}
+
+
 console.log(myLibrary)
 addBookToLibrary("Test Book", "Kyle", "300", true)
 console.log(myLibrary)
+
+myLibrary.forEach((book) => createCard(book))
